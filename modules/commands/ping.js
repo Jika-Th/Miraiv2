4ee45d38bca4ac108c72919f0dca984ec2d3528c
Jika-Th <91289461+Jika-Th@users.noreply.github.com>
2022-01-16 21:54:17 +0700

    Update ping.js

--- modules/commands/ping.js
+++ modules/commands/ping.js
@@ -13,7 +13,7 @@ module.exports.run = async function({ api, event, args }) {
 	try {
 		const botID = api.getCurrentUserID();
 		const listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
-		var body = (args.length != 0) ? args.join(" ") : "@everyone", mentions = [], index = 0;
+		var body = (args.length != 0) ? args.join(" ") : "@@@@@@@", mentions = [], index = 0;
 		
 		for(const idUser of listUserID) {
 			body = "‎" + body;
